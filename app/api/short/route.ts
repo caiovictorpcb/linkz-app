19c229496095203d9c87e442c335fc52eb6a459e
Caio Victor <caiovictor.pcb@gmail.com>
2025-03-09 02:05:43 -0300

    change server fetch

--- app/api/short/route.ts
+++ app/api/short/route.ts
@@ -5,13 +5,8 @@ const apiUrl = `${process.env.API_URL}`
 
 export async function POST(req: Request) {
   const { url } = await req.json()
-  const res = await fetch(`${apiUrl}/short`, {
-    method: 'POST',
-    body: JSON.stringify({ url }),
-  }).then((res) => res.json())
+  const res = await fetch(`${apiUrl}/short?url=${url}`).then((res) => res.json())
   const { shortUrl } = res
-  
-  
  
   return Response.json({ shortUrl })
 }
